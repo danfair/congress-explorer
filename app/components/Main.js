3188d8fa2c5c2f75899e55912def4e252432501d
Dan Fair <fair.dan@gmail.com>
2016-07-04 11:48:25 -0500

    initial commit

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h1>Main!</h1>
+				{this.props.children}
+			</div>
+		)
+	}
+});
+
+module.exports = Main;
+	
